fcb619e80157b8f368fab2ad1a3517a759eaf65a
mewhhaha <kopatheonlyone@hotmail.com>
2021-12-03 14:45:47 +0100

    Simplify solution

--- src/Advent3.hs
+++ src/Advent3.hs
@@ -11,31 +11,27 @@ import Data.Char (digitToInt)
 import Data.Either (partitionEithers)
 import Data.List (foldl')
 
-toBits :: [String] -> [[Bool]]
-toBits = (fmap . fmap) (== '1')
+parseBits :: [String] -> [[Bool]]
+parseBits = (fmap . fmap) (== '1')
 
 readBits :: IO [[Bool]]
-readBits = toBits . lines <$> readFile "./data/advent3.txt"
+readBits = parseBits . lines <$> readFile "./data/advent3.txt"
 
 number :: [Bool] -> Int
 number = foldl' (\acc x -> acc * 2 + if x then 1 else 0) 0
 
 {-
->>> solve1 (toBits ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]) == 198
+>>> solve1 (parseBits ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]) == 198
 True
 
 -}
 
 solve1 :: [[Bool]] -> Int
-solve1 bits = ((*) <$> epsilon <*> gamma) . number . clamp . foldl' count (repeat 0) $ bits
+solve1 = ((*) <$> epsilon <*> gamma) . fmap (> 0) . foldl' count (repeat 0)
   where
-    bitlen = length . head $ bits
-    bitmask = number $ replicate bitlen True
     count = zipWith (\acc x -> if x then acc + 1 else acc - 1)
-    clamp = fmap (> 0)
-
-    epsilon = id
-    gamma = (.&. bitmask) . complement
+    epsilon = number
+    gamma = number . fmap not
 
 run1 :: IO ()
 run1 = do
@@ -43,7 +39,7 @@ run1 = do
   print $ solve1 instructions
 
 {-
->>> solve2 (toBits ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]) == 230
+>>> solve2 (parseBits ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]) == 230
 True
 
 -}
@@ -51,14 +47,15 @@ True
 solve2 :: [[Bool]] -> Int
 solve2 = (*) <$> ogr <*> co2
   where
-    pickBy f cmp (os, zs) = if f os `cmp` f zs then os else zs
+    pick cmp (os, zs) = if length os `cmp` length zs then os else zs
 
     calc :: (Int -> Int -> Bool) -> [[Bool]] -> Int
     calc cmp bits = go (0, bits)
       where
         go (i, [x]) = number x
         go (i, xs) =
-          let pass = pickBy length cmp . partitionEithers . fmap (\n -> if n !! i then Left n else Right n)
+          let categorize f n = if f n then Left n else Right n
+              pass = pick cmp . partitionEithers . fmap (categorize (!! i))
            in go (i + 1, pass xs)
 
     ogr = calc (>=)
