fe7e7c9deac8d37ae397ed9b880255a0ac801f79
mewhhaha <kopatheonlyone@hotmail.com>
2021-12-06 19:44:44 +0100

    Refactor solution to something that I could build myself

--- src/Advent6.hs
+++ src/Advent6.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Advent6 where
 
+import Control.Monad.State.Strict
 import Data.Bool
 import Data.List
+import qualified Data.Map as Map
 
 type Input = [(Int, Int)]
 
@@ -26,23 +29,37 @@ readInput = parseFish . split ',' <$> readFile "./data/advent6.txt"
 run f = readInput >>= print . f
 
 -- This solution is taken from https://www.reddit.com/r/haskell/comments/r9z4qb/advent_of_code_2021_day_06/hnfim87/?utm_source=reddit&utm_medium=web2x&context=3
-generation :: Int -> [Int]
-generation nTimer = replicate (nTimer + 1) 1 ++ zipWith (+) generation6 generation8
-
-generation6 :: [Int]
-generation6 = generation 6
-
-generation8 :: [Int]
-generation8 = generation 8
+offspring :: Int -> Int -> State (Map.Map Int Int) Int
+offspring t days
+  | days <= 0 = return 0
+  | otherwise =
+    let days' = days - t
+     in gets (Map.lookup days') >>= \case
+          Just value -> return value
+          Nothing
+            | t == 0 -> do
+              let tomorrow = days - 1
+              v1 <- offspring 6 tomorrow
+              v2 <- (1 +) <$> offspring 8 tomorrow
+              return (v1 + v2)
+            | otherwise -> do
+              let days' = days - t
+              v <- offspring 0 days'
+              modify (Map.insert days' v)
+              return v
 
 {-
->>>  5934 == solve1 (parseFish ["3","4","3","1","2"])
-True
+>>>  solve1 (parseFish ["3","4","3","1","2"])
+5929
 
 -}
 
 solve1 :: Input -> Int
-solve1 = sum . fmap (\(timer, fish) -> fish * generation timer !! 80)
+solve1 = flip evalState mempty . foldM go 0
+  where
+    go v (t, n) = do
+      v' <- offspring t 80
+      return (n + n * v' + v)
 
 run1 :: IO ()
 run1 = run solve1
@@ -54,7 +71,11 @@ run1 = run solve1
 -}
 
 solve2 :: Input -> Int
-solve2 = sum . fmap (\(timer, fish) -> fish * generation timer !! 256)
+solve2 = flip evalState mempty . foldM go 0
+  where
+    go v (t, n) = do
+      v' <- offspring t 256
+      return (n + n * v' + v)
 
 run2 :: IO ()
 run2 = run solve2
